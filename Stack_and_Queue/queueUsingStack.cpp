9fda91a7fd4f431e1a197452079532111b6835d3
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2022-11-27 17:39:23 +0530

    Create queueUsingStack.cpp

---
+++ Stack_and_Queue/queueUsingStack.cpp
@@ -0,0 +1,38 @@
+class MyQueue {
+private:
+    stack<int> input, output;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        if(output.empty()){
+            while(input.empty() == false){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int ans = output.top();
+        output.pop();
+        return ans;
+    }
+    
+    int peek() {
+        if(output.empty()){
+            while(input.empty() == false){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
