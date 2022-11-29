0ccbdda2c547840acc995def3bf5e13cded50749
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2022-11-29 17:27:25 +0530

    Create allInOne.cpp

---
+++ Tree/allInOne.cpp
@@ -0,0 +1,26 @@
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> postOrder, inOrder, preOrder;
+    if(root == NULL) return postOrder;
+    stack<pair<TreeNode*, int>> st;
+    TreeNode* node = root;
+    while(true){
+        if(node != NULL){
+            preOrder.push_back(node->val);
+            st.push({node, 1});
+            node = node->left;
+        }else{
+            if(st.empty()) break;
+            TreeNode* temp = st.top().first;
+            int flag = st.top().second;
+            st.pop();
+            if(flag == 2){
+                postOrder.push_back(temp->val);
+            }else{
+                st.push({temp, 2});
+                inOrder.push_back(temp->val);
+                node = temp->right;
+            }
+        }
+    }
+    return postOrder;
+}
