f3b63e88da3c0d53f7095d0adb6d4789e4895add
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-25 17:16:54 +0530

    Update rotateRight.cpp

--- LinkedList and Arrays/rotateRight.cpp
+++ LinkedList and Arrays/rotateRight.cpp
@@ -1,23 +1,39 @@
-ListNode* rotateRight(ListNode* head, int k) {
-        if(!head) return head;
-        int len = 0;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    int getLength(ListNode* head) {
         ListNode* node = head;
-        while(node){
-            len++;
+        int cnt = 0;
+        while(node != nullptr) {
             node = node->next;
+            cnt++;
         }
-        k = k % len;
-        ListNode* ptr = head;
-        for(int i = 1; i <= k; i++){
-            ptr = ptr->next;
-        }
-        node = head;
-        while(ptr->next){
-            node = node->next;
-            ptr = ptr->next;
-        }
-        ptr->next = head;
-        head = node->next;
-        node->next = NULL;
-        return head;
+        return cnt;
     }
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        int listLength = getLength(head);
+        if (listLength == 0) return nullptr;
+        k = k % listLength;
+        if (k == 0) return head;
+        ListNode* node = head;
+        for (int i = 0; i < listLength-k-1; i++) {
+            node = node->next;
+        }
+        ListNode* node2 = node->next;
+        ListNode* newHead = node2;
+        while(node2->next != nullptr) node2 = node2->next;
+        node->next = nullptr;
+        node2->next = head;
+        return newHead;
+    }
+};
