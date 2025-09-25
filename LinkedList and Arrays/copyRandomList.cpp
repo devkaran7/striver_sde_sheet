b47a93be701b6b1df2626ec6fdba90a84b0b5368
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-25 18:06:44 +0530

    Update copyRandomList.cpp

--- LinkedList and Arrays/copyRandomList.cpp
+++ LinkedList and Arrays/copyRandomList.cpp
@@ -1,26 +1,45 @@
-Node* copyRandomList(Node* head) {
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> mp;
+    Node* getDeepCopy(Node* node) {
+        if (node == nullptr) return node;
+        if (mp.find(node) == mp.end()) {
+            mp[node] = new Node(node->val);
+        }
+        return mp[node];
+    }
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) return head;
         Node* node = head;
-        while(node != NULL){
-            Node* newNode = new Node(node->val);
-            newNode->next = node->next;
-            node->next = newNode;
-            node = node->next->next;
-        }
-        node = head;
-        while(node != NULL){
-            if(node->random != NULL){
-                node->next->random = node->random->next;
+        while(node != nullptr) {
+            Node* currentNode = getDeepCopy(node);
+            if (node->next != nullptr) {
+                Node* nextCurrentNode = getDeepCopy(node->next);
+                currentNode->next = nextCurrentNode;
             }
-            node = node->next->next;
-        }
-        Node* newHead = new Node(0);
-        Node* newNode = newHead;
-        node = head;
-        while(node != NULL){
-            newNode->next = node->next;
-            node->next = node->next->next;
-            newNode = newNode->next;
+            if (node->random != nullptr) {
+                Node* randomCurrentNode = getDeepCopy(node->random);
+                currentNode->random = randomCurrentNode;
+            }
             node = node->next;
         }
-        return newHead->next;
+        return getDeepCopy(head);
     }
+};
