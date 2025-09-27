30f549036d5b0672eb1ada8150e839f6c5ea6582
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-27 22:13:24 +0530

    Update searchinrotatedsortedarray.cpp

--- binary_search/searchinrotatedsortedarray.cpp
+++ binary_search/searchinrotatedsortedarray.cpp
@@ -1,24 +1,50 @@
-int search(vector<int>& arr, int x) {
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
         int low = 0;
-        int high = arr.size() -1;
-        while(high >= low){
-            int mid = (high + low)/2;
-            if(arr[mid] == x){
-                return mid;
-            }
-            if(arr[low] <= arr[mid]){
-                if(x <= arr[mid] && x >= arr[low]){
-                    high = mid -1;
-                }else{
+        int high = nums.size() - 1;
+        while(low <= high) {
+            int mid = (low + high) >> 1;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] >= nums[low]) {
+                // low to mid is sorted
+                if (target <= nums[mid] && target >= nums[low]) {
+                    high = mid - 1;
+                } else {
                     low = mid + 1;
                 }
-            }else{
-                if(x >= arr[mid] && x <= arr[high]){
+            } else {
+                // mid to high is sorted
+                if (target >= nums[mid] && target <= nums[high]) {
                     low = mid + 1;
-                }else{
+                } else {
                     high = mid - 1;
                 }
             }
         }
         return -1;
     }
+};
+
+/*
+
+4, 5, 6, 7, 0, 1, 2
+
+low -> 0 -> 4
+high -> 6 -> 2
+mid -> 3 -> 7
+target = 0
+
+low = 4 -> 0
+high = 6 -> 2
+mid = 5 -> 1
+target = 0
+
+low = 4
+high = 4
+mid = 4
+target = 0
+
+return 4
+
+*/
