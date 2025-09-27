ecde5040281a4b6245d905d5a82db4765e2bdcaf
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 00:39:36 +0530

    Update Top_K_Frequent_Elements.cpp

--- Heap/Top_K_Frequent_Elements.cpp
+++ Heap/Top_K_Frequent_Elements.cpp
@@ -1,21 +1,33 @@
-vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for(auto it : nums){
-            mp[it]++;
+class Solution {
+private:
+    struct Box {
+        int value;
+        int frequency;
+        Box(int value, int frequency) : value(value), frequency(frequency) {}
+    };
+    struct Compare {
+        bool operator()(const auto &a, const auto &b) const {
+            return a.frequency > b.frequency;
         }
-        priority_queue<pair<int, int>> pq;
-        for(auto it : mp){
-            if(pq.size() < k){
-                pq.push({-it.second, it.first});
-            }else if(it.second > -pq.top().first){
+    };
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> frequencyMap;
+        for (const auto &num : nums) {
+            frequencyMap[num]++;
+        }
+        priority_queue<Box, vector<Box>, Compare> pq;
+        for (const auto &kv : frequencyMap) {
+            pq.push(Box(kv.first, kv.second));
+            if (pq.size() > k) {
                 pq.pop();
-                pq.push({-it.second, it.first});
             }
         }
-        vector<int> arr;
-        while(pq.empty() == false){
-            arr.push_back(pq.top().second);
+        vector<int> answer;
+        while(pq.empty() == false) {
+            answer.push_back(pq.top().value);
             pq.pop();
         }
-        return arr;
+        return answer;
     }
+};
