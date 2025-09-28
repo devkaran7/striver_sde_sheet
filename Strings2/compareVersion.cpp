7e8063da13f77b906317ee19d088746b20bc3965
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 23:04:50 +0530

    Update compareVersion.cpp

--- Strings2/compareVersion.cpp
+++ Strings2/compareVersion.cpp
@@ -1,53 +1,35 @@
 class Solution {
 private:
-    int compare(string &a, string &b){
-        if(a.size() > b.size()){
-            return 1;
-        }else if(a.size() < b.size()){
-            return -1;
-        }
-        int i = 0;
-        while(i < a.size()){
-            if(a[i] > b[i]){
-                return 1;
-            }else if(a[i] < b[i]){
-                return -1;
-            }
+    int getNextInt(string &s, int &i) {
+        int integer = 0;
+        while(i < s.size() && s[i] != '.') {
+            integer *= 10;
+            integer += (s[i] - '0');
             i++;
         }
-        return 0;
+        if (i < s.size()) i++;
+        return integer;
     }
 public:
     int compareVersion(string version1, string version2) {
-        int n = version1.size();
-        int m = version2.size();
         int i = 0, j = 0;
-        while(i < n || j < m){
-            string s = "";
-            if(i < n){
-                while(i < n && version1[i] != '.'){
-                    if(version1[i] != '0' || s.size() != 0){
-                        s += version1[i];
-                    }
-                    i++;
-                }
-                i++;
-            }
-            string t = "";
-            if(j < m){
-                while(j < m && version2[j] != '.'){
-                    if(version2[j] != '0' || t.size() != 0){
-                        t += version2[j];
-                    }
-                    j++;
-                }
-                j++;
-            }
-            int answer = compare(s,t);
-            if(answer != 0){
-                return answer;
+        while(i < version1.size() || j < version2.size()) {
+            int first = getNextInt(version1, i);
+            int second = getNextInt(version2, j);
+            if (first < second) {
+                return -1;
+            } else if (second < first) {
+                return 1;
             }
         }
         return 0;
     }
 };
+
+/*
+
+v1 -> 1
+
+v2 -> 1
+
+*/
