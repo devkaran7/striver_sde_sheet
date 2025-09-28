d955f8b2078f232cafb67e61a2aa3f03efeeaaa2
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 20:55:14 +0530

    Update celebrity.cpp

--- Stack_and_Queues_part2/celebrity.cpp
+++ Stack_and_Queues_part2/celebrity.cpp
@@ -1,29 +1,20 @@
-int celebrity(vector<vector<int> >& matrix, int n) 
-    {
-        // code here 
-        stack<int> st;
-        for(int i = 0; i < n; i++){
-            st.push(i);
-        }
-        while(st.size() > 1){
-            int a = st.top();
-            st.pop();
-            int b = st.top();
-            st.pop();
-            if(matrix[a][b] == 1){
-                st.push(b);
-            }else{
-                st.push(a);
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int start = 0;
+        int end = n-1;
+        while(end > start) {
+            if (knows(start, end)) {
+                start++;
+            } else {
+                end--;
             }
         }
-        int possible_ans = st.top();
-        for(int i = 0; i < n; i++){
-            if(i == possible_ans) continue;
-            if(matrix[possible_ans][i] != 0) return -1;
+        
+        for (int i = 0; i < n; i++) {
+            if (i == start) continue;
+            if (knows(start, i) || !knows(i, start)) return -1;
         }
-        for(int i = 0; i < n; i++){
-            if(i == possible_ans) continue;
-            if(matrix[i][possible_ans] != 1) return -1;
-        }
-        return possible_ans;
+        return start;
     }
+};
