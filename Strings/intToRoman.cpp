d09681ef84448c770bd76463cb4d1b253a40420e
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 21:12:08 +0530

    Update intToRoman.cpp

--- Strings/intToRoman.cpp
+++ Strings/intToRoman.cpp
@@ -1,32 +1,66 @@
-private:
-    char c[10] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Solution {
 public:
-    string intToRoman(int num) {
-        int i = 0;
-        int temp = num;
-        string s = "";
-        while(temp){
-            string str = "";
-            int digit = temp % 10;
-            if(digit <= 3){
-                for(int j = 0; j < digit; j++){
-                    str += c[i];
-                }
-            }else if(digit == 4){
-                str += c[i];
-                str += c[i+1];
-            }else if(digit == 9){
-                str += c[i];
-                str += c[i+2];
-            }else{
-                str += c[i+1];
-                for(int j = 0; j < (digit%5); j++){
-                    str += c[i];
-                }
+    int romanToInt(std::string s) {
+        // Map to store the values of Roman numerals
+        std::unordered_map<char, int> values = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        // Initialize total with the value of the last character
+        int total = values[s.back()];
+        
+        // Iterate from the second-to-last character to the first
+        for (int i = s.length() - 2; i >= 0; --i) {
+            // If current value is less than the value to its right, subtract
+            if (values[s[i]] < values[s[i + 1]]) {
+                total -= values[s[i]];
+            } 
+            // Otherwise, add
+            else {
+                total += values[s[i]];
             }
-            temp = temp / 10;
-            i+=2;
-            s = str + s;
         }
-        return s;
+        
+        return total;
     }
+};
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::string intToRoman(int num) {
+        // Create lookup tables sorted from largest to smallest value
+        std::vector<int> values = {
+            1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+        };
+        std::vector<std::string> symbols = {
+            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+        
+        std::string result = "";
+        
+        // Greedily subtract the largest possible values
+        for (int i = 0; i < values.size(); ++i) {
+            // While the number is large enough for the current symbol
+            while (num >= values[i]) {
+                result += symbols[i];
+                num -= values[i];
+            }
+        }
+        
+        return result;
+    }
+};
