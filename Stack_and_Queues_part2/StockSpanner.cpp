d9d35f1ad2d01c83cebb2eae97361c6ae2cdec91
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 18:19:06 +0530

    Update StockSpanner.cpp

--- Stack_and_Queues_part2/StockSpanner.cpp
+++ Stack_and_Queues_part2/StockSpanner.cpp
@@ -1,18 +1,37 @@
 class StockSpanner {
 private:
+    // {value, index}
     stack<pair<int, int>> st;
+    int index;
 public:
     StockSpanner() {
-        
+        index = 0;
     }
     
     int next(int price) {
-        int cnt = 1;
-        while(st.empty() == false && st.top().first <= price){
-            cnt += st.top().second;
+        while(!st.empty() && st.top().first <= price) {
             st.pop();
         }
-        st.push({price, cnt});
-        return cnt;
+        int prevGreaterIndex = (st.empty()) ? -1 : st.top().second;
+        st.push(make_pair(price, index));
+        int count = index - prevGreaterIndex;
+        index++;
+        return count;
     }
 };
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
+ /*
+ 
+ 100, 0, 80, 60, 70
+
+ stack -> {100, 80, 70}
+
+ 100, 80, 60, 70, 70, 75, 85
+
+ */
