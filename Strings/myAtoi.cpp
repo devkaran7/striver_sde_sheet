27b71a228642e4e0b6d30ec020f4e4e8cffdb1d4
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 21:23:58 +0530

    Update myAtoi.cpp

--- Strings/myAtoi.cpp
+++ Strings/myAtoi.cpp
@@ -1,48 +1,34 @@
 class Solution {
-private:
-    int getNum(string &s, bool isNeg){
-        long long x = isNeg ? -1 : 1;
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+        while(i < n && s[i] == ' '){
+            i++;
+        }
+        if(i == n){
+            return 0;
+        }
+        bool pos = true;
+        if(s[i] == '+'){
+            pos = true;
+            i++;
+        }else if(s[i] == '-'){
+            pos = false;
+            i++;
+        }
         long long ans = 0;
-        for(int i = s.size() - 1; i >= 0; i--){
-            ans = ans + (s[i] - '0') * x;
-            if(ans >= INT_MAX || x >= INT_MAX){
-                return INT_MAX;
-            }else if(ans <= INT_MIN || x <= INT_MIN){
-                return INT_MIN;
-            }
-            x = x * 10ll;
+        while(i < n && s[i] - '0' <= 9 && s[i] - '0' >= 0){
+            ans = ans * 10ll + (s[i] - '0');
+            if (pos && ans >= INT_MAX) return INT_MAX;
+            if (!pos && -ans <= INT_MIN) return INT_MIN;
+            i++;
+        }
+        if(pos == false){
+            ans = -ans;
         }
         return (int) ans;
     }
-public:
-    int myAtoi(string s) {
-        int ind = 0;
-        int n = s.size();
-        string result = "";
-        int neg = 0;
-        while(ind < n){
-            if((s[ind] == '-' || s[ind] == '+') && (neg == 0)){
-                if(s[ind] == '+'){
-                    neg++;
-                }
-                neg++;
-            }else if(s[ind] == '0'){
-                if(result.size() != 0){
-                    result += s[ind];
-                }else{
-                    neg += 2;
-                }
-            }else if(s[ind] - '0' <= 9 && s[ind] - '0' >= 1){
-                result += s[ind];
-                neg += 2;
-            }else if(s[ind] == ' ' && result.size() == 0 && neg == 0){
-                ind++;
-                continue;
-            }else{
-                break;
-            }
-            ind++;
-        }
-        return getNum(result, neg%2);
-    }
 };
+
+// INT_MAX and INT_MIN abs value is not same
