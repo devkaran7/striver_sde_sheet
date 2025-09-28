42859ef472c2906d489ee75ecfaf5c7571e8d847
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-09-28 21:43:38 +0530

    Update repeatedStringMatch.cpp

--- Strings/repeatedStringMatch.cpp
+++ Strings/repeatedStringMatch.cpp
@@ -1,61 +1,50 @@
 class Solution {
 private:
-    long long mod = 1e9 + 7;
-    long long power(long long base, long long x){
-        long long ans = 1;
-        while(x){
-            if(x % 2 == 0){
-                base = (base * base) % mod;
-                x = x / 2;
-            }else{
-                ans = (ans * base) % mod;
-                x--;
-            }
+    long long prime;
+    long long mod;
+    bool rabinKarp(string &s, string &pattern) {
+        int n = s.size();
+        int m = pattern.size();
+        long long hash1 = 0;
+        long long hash2 = 0;
+        long long currentPrime = 1;
+        for (int i = 0; i < m; i++) {
+            hash1 = (hash1 + ((s[i] - 'a' + 1) * currentPrime) % mod) % mod;
+            hash2 = (hash2 + ((pattern[i] - 'a' + 1) * currentPrime) % mod) % mod;
+            currentPrime = (currentPrime * prime) % mod;
         }
-        return ans;
-    }
-    long long blackbox(string &s){
-        long long stringHash = 0;
-        long long pr = 1;
-        for(int i = 0; i < s.size(); i++){
-            stringHash = (stringHash + ((s[i] - 'a'+ 1ll) * pr) % mod) % mod;
-            pr = (pr * 31ll) % mod;
-        }
-        return stringHash;
-    }
-    bool rabin_karp(string &str, string &ptr){
-        int n = str.size();
-        int m = ptr.size();
-        long long patternHash = blackbox(ptr);
-        string temp = str.substr(0, m);
-        long long stringHash = blackbox(temp);
-        long long rightPrime = power(31, m);
-        long long leftPrime = 1;
-        if(patternHash == stringHash) return true;
-        for(int i = m; i < n; i++){
-            int nextIndex = i;
-            int prevIndex = i-m;
-            stringHash = (stringHash - (((str[prevIndex] - 'a' + 1) * leftPrime) % mod) + mod) % mod;
-            stringHash = (stringHash + ((str[nextIndex] - 'a' + 1) * rightPrime) % mod) % mod;
-            patternHash = (patternHash * 31ll) % mod;
-            if(stringHash == patternHash) return true;
-            rightPrime = (rightPrime * 31ll) % mod;
-            leftPrime = (leftPrime * 31ll) % mod;
+        if (hash1 == hash2) return true;
+        long long lastPrime = 1;
+        for (int i = m; i < n; i++) {
+            hash1 = (hash1 + ((s[i] - 'a' + 1) * currentPrime) % mod) % mod;
+            hash1 = (hash1 - ((s[i - m] - 'a' + 1) * lastPrime) % mod + mod) % mod;
+            hash2 = (hash2 * prime) % mod;
+            if (hash1 == hash2) return true;
+            currentPrime = (currentPrime * prime) % mod;
+            lastPrime = (lastPrime * prime) % mod;
         }
         return false;
     }
 public:
+    Solution() : prime(31), mod(1e9 + 7) {}
     int repeatedStringMatch(string a, string b) {
-        //use rabin karp
         string s = a;
         int count = 1;
-        while(s.size() < b.size()){
+        while(s.size() < b.size()) {
             s = s + a;
             count++;
         }
-        if(rabin_karp(s, b)) return count;
+        if (rabinKarp(s, b)) return count;
         s += a;
-        if(rabin_karp(s, b)) return count + 1;
+        count++;
+        if (rabinKarp(s, b)) return count;
         return -1;
     }
 };
+
+/*
+
+TC -> O(m)
+SC -> O(m)
+
+*/
