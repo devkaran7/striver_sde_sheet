a7899dc33728ac940b8f9975894087a340b8ad84
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-10-02 01:14:56 +0530

    Update isBalanced.cpp

--- Tree2/isBalanced.cpp
+++ Tree2/isBalanced.cpp
@@ -1,16 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 private:
-    int getHeight(TreeNode* root, bool &flag){
-        if(root == NULL) return -1;
-        int left = getHeight(root->left, flag);
-        int right = getHeight(root->right, flag);
-        flag = flag & (abs(left - right) <= 1);
+    bool flag;
+    int dfs(TreeNode* node) {
+        if (node == nullptr) return 0;
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        if (abs(left - right) > 1) flag = false;
         return 1 + max(left, right);
     }
 public:
     bool isBalanced(TreeNode* root) {
-        bool flag = true;
-        getHeight(root, flag);
+        flag = true;
+        dfs(root);
         return flag;
     }
 };
