07795669b313ae7c08b445a60753908f827d0ac0
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-10-02 21:03:58 +0530

    Update morrisTraversalInorder.cpp

--- Tree/morrisTraversalInorder.cpp
+++ Tree/morrisTraversalInorder.cpp
@@ -1,24 +1,39 @@
-vector<int> inorderTraversal(TreeNode* root) {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
         vector<int> inorder;
         TreeNode* node = root;
-        while(node != NULL){
-            if(node->left == NULL){
+        while (node != nullptr) {
+            cout << node->val << endl;
+            if (node->left == nullptr) {
                 inorder.push_back(node->val);
                 node = node->right;
-            }else{
-                TreeNode* temp = node->left;
-                while(temp->right != NULL && temp->right != node){
-                    temp = temp->right;
+            } else {
+                TreeNode* rightMostNode = node->left;
+                while(rightMostNode->right != nullptr && rightMostNode->right != node) {
+                    rightMostNode = rightMostNode->right;
                 }
-                if(temp->right == NULL){
-                    temp->right = node;
-                    node = node->left;
-                }else{
-                    temp->right = NULL;
+                if (rightMostNode->right == node) {
+                    rightMostNode->right = nullptr;
                     inorder.push_back(node->val);
                     node = node->right;
+                } else {
+                    rightMostNode->right = node;
+                    node = node->left;
                 }
             }
         }
         return inorder;
     }
+};
