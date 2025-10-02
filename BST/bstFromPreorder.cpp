f29e3ade488a86ef011a612841adbb2b89cba585
Dev Karan Singh <85109774+devkaran7@users.noreply.github.com>
2025-10-02 16:50:48 +0530

    Update bstFromPreorder.cpp

--- BST/bstFromPreorder.cpp
+++ BST/bstFromPreorder.cpp
@@ -1,17 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 private:
-    TreeNode* f(vector<int> &arr, int ub, int &i){
-        if(i == arr.size() || arr[i] > ub){
-            return NULL;
-        }
-        TreeNode* node = new TreeNode(arr[i++]);
-        node->left = f(arr, node->val, i);
-        node->right = f(arr, ub, i);
+    TreeNode* build(vector<int> &preorder, int &i, int upperBound) {
+        if (i >= preorder.size() || preorder[i] >= upperBound) return nullptr;
+        TreeNode* node = new TreeNode(preorder[i++]);
+        node->left = build(preorder, i, node->val);
+        node->right = build(preorder, i, upperBound);
         return node;
     }
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         int i = 0;
-        return f(preorder, INT_MAX, i);
+        return build(preorder, i, INT_MAX);
     }
 };
+
+/*
+
+dry run -> [8, 5, 1, 7, 10, 12]
+i = 0, up = IM
+8 -> left = 5
+5 -> left = 1
+1 -> left = null
+1 -> right = null
+5 -> right = 7
+7 -> left = null
+7 -> right = null
+8 -> right = 10
+10 -> left = null
+10 -> right = 12
+12 -> left = null
+12 -> right = null
+
+TC -> O(N)
+SC -> O(N)
+
+*/
